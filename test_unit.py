1b1672ac2c6dcf35e0630b3b4c466ecad35e3cce
jeffthomasweb <93561509+jeffthomasweb@users.noreply.github.com>
2021-12-02 16:17:22 -0500

    Update test_unit.py

--- test_unit.py
+++ test_unit.py
@@ -1,9 +1,10 @@
 from unittest import IsolatedAsyncioTestCase
 import asyncio
-from hackernews import hnews
+from hacker_news_best_stories_list.py import hnews
 
 class TestHnews(IsolatedAsyncioTestCase):
 
+    #Test that the async function hnews() returns a list of 20 items if we request 20 stories.
     async def test_async(self):
         list_title_json = await hnews(20)
         self.assertEqual(len(list_title_json), 20)
